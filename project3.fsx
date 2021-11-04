8cd5edd0cd34028cb310289e54d3a7df0bc246e2
Akshay Ganapathy <akshayg1996@gmail.com>
2021-11-03 20:57:00 -0400

    updated code

--- project3.fsx
+++ project3.fsx
@@ -9,256 +9,227 @@ open Akka.Actor
 
 
 let r = System.Random()
-let system = System.create "my-system" (Configuration.load())
-
-type Record =
-    {
-        InfoString: string
-        Key: int
-    }
+let system = System.create "system" (Configuration.load())
 
 
-type NodeInfo = 
-    {
-        NodeKey: int
-        NodeRef: IActorRef
-    }
-    
-
-type SetupInfo =
-    {
-        SelfKey: int
-        SNodeInfo: NodeInfo
-        PNodeInfo: NodeInfo
-        FNodeInfo: List<NodeInfo>
-    }
-
-
-type FixFingerInfo =
-    {
-        FingerKey: int
-        FingerIndex: int
-        FingerFrom: IActorRef
-        IsReturn: bool
-        TargetIndex: int
-        TargetNode: IActorRef
-    }
-
-
-type InfoToJoin =
-    {
-        Preceding: NodeInfo
-        Successor: NodeInfo
-    }
-
-
-type UpdatePreceding =
-    {
-        UPreceding: NodeInfo
-    }
-
-
-type queryMessage =
+type QueryMsg =
     {
         Source: IActorRef
-        Key: int
+        k: int
         Hop: int
     }
 
+type FixfngrInfo =
+    {
+        TargetIndex: int
+        TargetNode: IActorRef
+        FingerFrom: IActorRef
+        FingerKey: int
+        FingerIndex: int
+        IsReturn: bool
+    }
 
-let sha1 (s:string) =
+type InfoNd = 
+    {
+        Ndkey: int
+        NdRef: IActorRef
+    }
+
+type InfoToJoin =
+    {
+        Before: InfoNd
+        Next: InfoNd
+    }
+
+
+type UpdateBefore =
+    {
+        UBefore: InfoNd
+    }
+
+    
+type SetupInfo =
+    {
+        SelfKey: int
+        BInfoNd: InfoNd
+        NInfoNd: InfoNd
+        TableInfoNd: List<InfoNd>
+    }
+
+
+let shafunc (s:string) =
     let b = System.Text.Encoding.ASCII.GetBytes(s)
-    let bsha1 = System.Security.Cryptography.SHA1.Create().ComputeHash(b)
-    let mutable output = 0
-    for i in 0 .. (bsha1.Length - 1) do
-        output <- output + (int bsha1.[i])
-    if output < 0 then
-        output <- 0 - output
-    output
+    let bshafunc = System.Security.Cryptography.SHA1.Create().ComputeHash(b)
+    let mutable res = 0
+    for i in 0 .. (bshafunc.Length - 1) do
+        res <- res + (int bshafunc.[i])
+    if res < 0 then
+        res <- 0 - res
+    res
 
-
-// [<EntryPoint>]
-// let main argv =
 let args : string array = fsi.CommandLineArgs |>  Array.tail
-let mutable numNodes = 256
-let mutable numRequests = 8
+let mutable numNds = 200
+let mutable numRqsts = 6
 let mutable m = 10
 if args.Length > 0 then
-    numNodes <- int args.[0]
-    numRequests <- int args.[1]
+    numNds <- int args.[0]
+    numRqsts <- int args.[1]
     m <- 0
-    let mutable n = numNodes
+    let mutable n = numNds
     while n > 0 do
         m <- m + 1
         n <- (n >>> 1)
 
 
-let maxNum = 1 <<< m
-printfn "Task with %d nodes, %d requests, allocated max key of chord ring: %d" numNodes numRequests maxNum
+let maxN = 1 <<< m
 
-let chordActor (mailbox: Actor<_>) =
-    let mutable Key = 0
-    let mutable successor = {NodeKey = 0; NodeRef = mailbox.Self}
-    let mutable preceding = {NodeKey = 0; NodeRef = mailbox.Self}
+let chrngActor (mailbox: Actor<_>) =
     let mutable fingerTable = []
     let mutable fingerArray = [||]
-    let index = 0
-    let recordArray = 
-        [| 
-            for i in 0 .. 16 do
-                yield {InfoString = ""; Key = -1}
-        |]
+    let mutable k = 0
+    let mutable next = {Ndkey = 0; NdRef = mailbox.Self}
+    let mutable before = {Ndkey = 0; NdRef = mailbox.Self}
+    let closestbeforeNd id =
 
-    let closestPrecedingNode id =
-        //printfn "%d %d %d" Key preceding.NodeKey successor.NodeKey
-        //System.Threading.Thread.Sleep(500)
-        let mutable tmp = -1
+        let mutable temp = -1
         for i = m-1 downto 0 do
-            let idd = if id < Key then id + maxNum else id
-            let a = if fingerTable.[i].NodeKey < Key then fingerTable.[i].NodeKey + maxNum else fingerTable.[i].NodeKey
-            //printfn "%d %d" a idd
-            if tmp = -1 && a > Key && a < idd then
-                tmp <- i
-        //printfn "%d" tmp
-        if tmp = -1 then
+            let a = if fingerTable.[i].Ndkey < k then fingerTable.[i].Ndkey + maxN else fingerTable.[i].Ndkey
+            let id1 = if id < k then id + maxN else id
+            if temp = -1 && a > k && a < id1 then
+                temp <- i
+        if temp = -1 then
             mailbox.Self
         else 
-            fingerTable.[tmp].NodeRef
+            fingerTable.[temp].NdRef
        
-
-
     let rec loop() = actor {
         let! message = mailbox.Receive()
-        let sender = mailbox.Sender()
         match box message with
         | :? int as id -> 
-        (*
-            if id > Key && id <= successor.NodeKey then
-                printfn "found! %d %d" Key successor.NodeKey
-        *)
-            let target = closestPrecedingNode id
+            let target = closestbeforeNd id
             if (target) = mailbox.Self then
-                printfn "found! %d %d" Key successor.NodeKey
-
+                  printfn ""
             else
                 target <! id
-        | :? queryMessage as q ->
-            let target = closestPrecedingNode q.Key
+
+        | :? QueryMsg as q ->
+            let target = closestbeforeNd q.k
             if (target) = mailbox.Self then
-                printfn "found! %d %d" Key successor.NodeKey
-                q.Source <! q
+
+                  q.Source <! q
             else
-                target <! {Key = q.Key; Hop = q.Hop + 1; Source = q.Source}
+                target <! {k = q.k; Hop = q.Hop + 1; Source = q.Source}
+
+        | :? InfoToJoin as itj ->
+            before <- itj.Before
+            next <- itj.Next
+            for i = 0 to m - 1 do
+                next.NdRef <! {FingerKey = k + (1 <<< i);FingerIndex = i; FingerFrom = mailbox.Self; IsReturn = false;TargetIndex = -1; TargetNode = mailbox.Self}
+        
         | :? SetupInfo as info ->
-            Key <- info.SelfKey
-            successor <- info.SNodeInfo
-            preceding <- info.PNodeInfo
-            fingerTable <- info.FNodeInfo
-        | :? string as x -> 
-            printfn "%A" x
-        | :? NodeInfo as info ->
-            let target = closestPrecedingNode info.NodeKey
+            k <- info.SelfKey
+            next <- info.BInfoNd
+            before <- info.NInfoNd
+            fingerTable <- info.TableInfoNd
+
+        | :? InfoNd as info ->
+            let target = closestbeforeNd info.Ndkey
             if target = mailbox.Self then
-                printfn "found! %d %d" Key successor.NodeKey
-                let itj = {Preceding = {NodeKey = Key; NodeRef = mailbox.Self}; Successor = {NodeKey = successor.NodeKey; NodeRef = successor.NodeRef}}
-                info.NodeRef <! itj
-                successor.NodeRef <! {UPreceding = info}
-                successor <- info
+                let itj = {Before = {Ndkey = k; NdRef = mailbox.Self}; Next = {Ndkey = next.Ndkey; NdRef = next.NdRef}}
+                info.NdRef <! itj
+                next.NdRef <! {UBefore = info}
+                next <- info
                 for i = 0 to m - 1 do
-                    successor.NodeRef <! {FingerKey = Key + (1 <<< i);FingerIndex = i; FingerFrom = mailbox.Self; IsReturn = false;TargetIndex = -1; TargetNode = mailbox.Self}
+                    next.NdRef <! {FingerKey = k + (1 <<< i);FingerIndex = i; FingerFrom = mailbox.Self; IsReturn = false;TargetIndex = -1; TargetNode = mailbox.Self}
             else
                 target <! info
-        | :? UpdatePreceding as u ->
-            preceding <- u.UPreceding
-        | :? InfoToJoin as itj ->
-            preceding <- itj.Preceding
-            successor <- itj.Successor
-            for i = 0 to m - 1 do
-                successor.NodeRef <! {FingerKey = Key + (1 <<< i);FingerIndex = i; FingerFrom = mailbox.Self; IsReturn = false;TargetIndex = -1; TargetNode = mailbox.Self}
-        | :? FixFingerInfo as fi ->
+        
+        | :? FixfngrInfo as fi ->
             if fi.IsReturn = false then
-                let target = closestPrecedingNode fi.FingerKey
+                let target = closestbeforeNd fi.FingerKey
                 if target = mailbox.Self then
-                    printfn "found! %d %d" Key successor.NodeKey
-                    fi.FingerFrom <! {FingerFrom = fi.FingerFrom; FingerKey = fi.FingerKey; FingerIndex = fi.FingerIndex; IsReturn = true; TargetIndex = successor.NodeKey; TargetNode = successor.NodeRef}
+                    fi.FingerFrom <! {FingerFrom = fi.FingerFrom; FingerKey = fi.FingerKey; FingerIndex = fi.FingerIndex; IsReturn = true; TargetIndex = next.Ndkey; TargetNode = next.NdRef}
                 else
                     target <! fi
             else
-                Array.set fingerArray fi.FingerIndex {NodeKey = fi.TargetIndex; NodeRef = fi.TargetNode}
+                Array.set fingerArray fi.FingerIndex {Ndkey = fi.TargetIndex; NdRef = fi.TargetNode}
+        
+        | :? string as x -> 
+            printfn "%A" x
+
+        | :? UpdateBefore as u ->
+            before <- u.UBefore
+       
         | _ -> ()
             
         return! loop()
     }
     loop()
 
-let rands = [
-    for i in 1 .. numNodes do
-        yield (r.Next() % maxNum)
+let rndnumlst = [
+    for i in 1 .. numNds do
+        yield (r.Next() % maxN)
 ]
-let numList = List.sort rands
+let numlst = List.sort rndnumlst
 
-let actorList = [
-    for i in 0 .. (numNodes - 1) do
+let chrdactrlist = [
+    for i in 0 .. (numNds - 1) do
         let name = "Actor" + i.ToString()
-        let temp = spawn system name chordActor
+        let temp = spawn system name chrngActor
         yield temp
 ]
 
-for i in 0 .. (numNodes - 1) do
+for i in 0 .. (numNds - 1) do
     let FList = [
         for j in 0 .. (m-1) do
-            let tmp = 1 <<< j
-            let mutable mark = -1;
-            for k in 1 .. numNodes do
-                let a = if i + k >= numNodes then numList.[(i+k) % numNodes] + maxNum else numList.[(i+k) % numNodes]
-                if a >= tmp + numList.[i] && mark = -1 then
-                    mark <- (i+k) % numNodes
-            let temp = {NodeKey = numList.[mark]; NodeRef = actorList.[mark]}
+            let mutable mrk = -1;
+            let temp = 1 <<< j
+            for k in 1 .. numNds do
+                let a = if i + k >= numNds then numlst.[(i+k) % numNds] + maxN else numlst.[(i+k) % numNds]
+                if a >= temp + numlst.[i] && mrk = -1 then
+                    mrk <- (i+k) % numNds
+            let temp = {Ndkey = numlst.[mrk]; NdRef = chrdactrlist.[mrk]}
             yield temp
     ]
      
-    let myInfo = {SelfKey = numList.[i]; SNodeInfo = {NodeKey = numList.[(i+1) % numNodes]; NodeRef = actorList.[(i+1) % numNodes]}; PNodeInfo = {NodeKey = numList.[(i+numNodes-1) % numNodes]; NodeRef = actorList.[(i+numNodes-1) % numNodes]}; FNodeInfo = FList}
-    actorList.[i] <! myInfo
+    let myInfo = {SelfKey = numlst.[i]; BInfoNd = {Ndkey = numlst.[(i+1) % numNds]; NdRef = chrdactrlist.[(i+1) % numNds]}; NInfoNd = {Ndkey = numlst.[(i+numNds-1) % numNds]; NdRef = chrdactrlist.[(i+numNds-1) % numNds]}; TableInfoNd = FList}
+    chrdactrlist.[i] <! myInfo
 
 
-System.Threading.Thread.Sleep(1000)
-
-
-let inputStringList = [
-    for i in 1 .. numNodes do
-        let requestList = [
-            for j in 1 .. numRequests do
+let inpStrList = [
+    for i in 1 .. numNds do
+        let rqstList = [
+            for j in 1 .. numRqsts do
+                let mutable str = ""
                 let l = r.Next() % 100 + 1
-                let mutable s = ""
                 for k in 0 .. l do
-                    s <- s + char(r.Next() % 95 + 32).ToString()
-                yield s
+                    str <- str + char(r.Next() % 95 + 32).ToString()
+                yield str
         ]
-        yield requestList
+        yield rqstList
 ]
 
 
-let dispatcher = 
-    spawn system "dispatcher" 
+let BossActor = 
+    spawn system "BossActor" 
         (fun mailbox ->
-            let mutable counter = 0
-            let mutable total = 0
+            let mutable tot = 0
+            let mutable cntr = 0
             let rec loop() = actor {
                 let! message = mailbox.Receive()
-                let sender = mailbox.Sender()
                 match box message with
                 | :? string as s ->
-                    if s = "start" then
-                        for i in 0 .. (numNodes - 1) do
-                            for j in 0 .. (numRequests - 1) do
-                                
-                                actorList.[i] <! {Key = (sha1 inputStringList.[i].[j]) % maxNum; Hop = 0; Source = mailbox.Self}
-                | :? queryMessage as q ->
-                    total <- total + q.Hop
-                    counter <- counter + 1
-                    if counter = numNodes * numRequests then
-                        printfn "Average Hops: %f" ((float total) / (float counter))
-                        printfn "Total Number of Hops: %f" ((float total))
+                    if s = "begin" then
+                        for i in 0 .. (numNds - 1) do
+                            for j in 0 .. (numRqsts - 1) do
+                                chrdactrlist.[i] <! {k = (shafunc inpStrList.[i].[j]) % maxN; Hop = 0; Source = mailbox.Self}
+
+                | :? QueryMsg as q ->
+                    tot <- tot + q.Hop
+                    cntr <- cntr + 1
+                    if cntr = numNds * numRqsts then
+                        printfn "Average Hops: %f" ((float tot) / (float cntr))
+                        printfn "Total number of hops: %f" ((float tot))
                 | _ -> ()
 
                 return! loop()
@@ -267,15 +238,9 @@ let dispatcher =
         )
 
 
-printfn "Start!"
-System.Threading.Thread.Sleep(500)
-//actorList.[23] <! 87
-dispatcher <! "start"
-
+printfn "Boss started"
+BossActor <! "begin"
     
 System.Console.ReadLine() |> ignore
 
 
-
-
-//0// return an integer exit code
